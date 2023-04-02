bd7db1cf09c045755d7c3ced8267189585391b4b
Ayala 0x3c [ Rukan ] <71002636+Darky-Oss@users.noreply.github.com>
2023-04-02 14:55:48 -0300

    Update restart_maquina.asm

--- restart_maquina.asm
+++ restart_maquina.asm
@@ -9,8 +9,3 @@ _start:
     mov byte ptr [0xcfb], 0x00
     jmp $
  
- ; Provavelmente o código não rose em sua máquina 
- ; Esse código é apenas para fins de educação.
-
- ; 0xcf9 -> usado para controlar o reset da máquina
- ; 0x06 -> escrito no endereço de 0xcf9
